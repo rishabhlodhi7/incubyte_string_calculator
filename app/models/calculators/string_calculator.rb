46b04e9f5f227e9a8b5664915c0c91fd0ade049b
rishabh <rishabhlodhi9@gmail.com>
2024-05-29 12:31:41 +0530

    adds third test case and method

--- app/models/calculators/string_calculator.rb
+++ app/models/calculators/string_calculator.rb
@@ -3,6 +3,7 @@ module Calculators
     def add(numbers)
       return 0 if numbers.empty?
       return numbers.to_i if numbers.to_i.to_s == numbers
+      numbers.split(',').map(&:to_i).reduce(0, :+)
     end
   end
 end
--- spec/models/calculators/string_calculator_spec.rb
+++ spec/models/calculators/string_calculator_spec.rb
@@ -11,6 +11,11 @@ RSpec.describe Calculators::StringCalculator, type: :model do
       calculator = Calculators::StringCalculator.new
       expect(calculator.add('1')).to eq(1)
     end
+
+    it 'returns the sum of two numbers' do
+      calculator = Calculators::StringCalculator.new
+      expect(calculator.add('1,2')).to eq(3)
+    end
   end
 end
 
