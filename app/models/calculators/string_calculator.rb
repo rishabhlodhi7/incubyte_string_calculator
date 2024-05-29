df03c8dd52cec517f770c2d8ec461eb0eef4bd5a
rishabh <rishabhlodhi9@gmail.com>
2024-05-29 12:30:09 +0530

    adds second test case and method

--- app/models/calculators/string_calculator.rb
+++ app/models/calculators/string_calculator.rb
@@ -2,6 +2,7 @@ module Calculators
   class StringCalculator
     def add(numbers)
       return 0 if numbers.empty?
+      return numbers.to_i if numbers.to_i.to_s == numbers
     end
   end
 end
--- spec/models/calculators/string_calculator_spec.rb
+++ spec/models/calculators/string_calculator_spec.rb
@@ -6,5 +6,11 @@ RSpec.describe Calculators::StringCalculator, type: :model do
       calculator = Calculators::StringCalculator.new
       expect(calculator.add('')).to eq(0)
     end
+
+    it 'returns the number itself for a single number' do
+      calculator = Calculators::StringCalculator.new
+      expect(calculator.add('1')).to eq(1)
+    end
   end
 end
+
